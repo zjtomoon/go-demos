63364972dd153fc2bbd0c27b6f780b080de8f54b
zjtomoon <zjtomoon@outlook.com>
2022-02-02 08:56:54 +0800

    default commit

---
+++ study/leetcode/lc45/main.go
@@ -0,0 +1,21 @@
+package main
+
+func jump(nums []int) int {
+	var step int
+	var curDistance int
+	var nextDistance int
+	if len(nums) == 0 || len(nums) == 1 {
+		step = 0
+	}
+	for i := 0 ; i < len(nums); i++ {
+
+	}
+	return step
+}
+
+func max(a,b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
