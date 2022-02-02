6d20cdcb3fb96bf82d1776366454521d807ef508
zjtomoon <zjtomoon@outlook.com>
2022-02-02 18:11:52 +0800

    跳跃游戏-贪心算法

--- study/leetcode/lc45/main.go
+++ study/leetcode/lc45/main.go
@@ -1,10 +1,11 @@
 package main
 
+// 跳跃游戏II
 func jump(nums []int) int {
 	var step int
 	var curDistance int
 	var nextDistance int
-	if len(nums) == 0 || len(nums) == 1 {
+	if len(nums) == 1 {
 		step = 0
 	}
 	for i := 0 ; i < len(nums); i++ {
