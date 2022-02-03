d0ec515290e8c98645caaebda9435a14c2ea16ed
zjtomoon <zjtomoon@outlook.com>
2022-02-03 09:38:06 +0800

    跳跃游戏-贪心算法

--- study/leetcode/lc45/main.go
+++ study/leetcode/lc45/main.go
@@ -5,16 +5,20 @@ func jump(nums []int) int {
 	var step int
 	var curDistance int
 	var nextDistance int
-	if len(nums) == 1 {
+	if len(nums) <= 1 {
 		step = 0
 	}
-	for i := 0 ; i < len(nums); i++ {
-
+	for i := 0; i < len(nums) - 1; i++ { // 循环到 len(nums)-2
+		nextDistance = max(nextDistance, i+nums[i]) //更新下一步覆盖的
+		if i == curDistance { //初始位置 i = 0 curDistance = 0
+			curDistance = nextDistance
+			step++
+		}
 	}
 	return step
 }
 
-func max(a,b int) int {
+func max(a, b int) int {
 	if a > b {
 		return a
 	}
