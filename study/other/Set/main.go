649159d7c35323a86fc9a8c6032f8f1067cf04b9
zjtomoon <zjtomoon@outlook.com>
2022-02-11 11:51:25 +0800

    default commit

--- study/other/Set/main.go
+++ study/other/Set/main.go
@@ -2,26 +2,24 @@ package main
 
 import "sync"
 
-
-type Set struct{
-  m map[int]bool
-  sync.Mutex
+type Set struct {
+	m map[int]bool
+	sync.Mutex
 }
-
 
 func New() *Set {
-  return &Set{
-    m: map[int]bool{},
-  }
+	return &Set{
+		m: map[int]bool{},
+	}
 }
 
-func (s *Set) Add(item int)  {
-  s.Lock()
-  defer s.Unlock()
-  s.m[item] = true
+func (s *Set) Add(item int) {
+	s.Lock()
+	defer s.Unlock()
+	s.m[item] = true
 }
 
 // 模拟实现set
-func main()  {
-  
+func main() {
+
 }
