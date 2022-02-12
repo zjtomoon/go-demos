7c07edc2a52a50cea2e90ab22ba4fbc37db8d112
zjtomoon <zjtomoon@outlook.com>
2022-02-12 20:11:14 +0800

    default commit

--- study/newcoder/HJ/HJ58/main.go
+++ study/newcoder/HJ/HJ58/main.go
@@ -1,9 +1,15 @@
 package main
 
-import "bufio"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main()  {
-  scan := bufio.NewScanner()
+  scan := bufio.NewScanner(os.Stdin)
   for {
     scan.Scan()
     input1 := scan.Text()
@@ -12,7 +18,16 @@ func main()  {
     }
 
     scan.Scan()
-    input2 := scan.Scan()
-
+    input2 := scan.Text()
+    input1_split := strings.Split(input1," ")
+    if len(input1_split) < 2 {
+      continue
+    }
+    input2_split := strings.Split(strings.Trim(input2," ")," ")
+    n,_ := strconv.Atoi(input1_split[0])
+    k,_ := strconv.Atoi(input1_split[1])
+    if n == 0 || k == 0 {
+      fmt.Println(0)
+    }
   }
 }
