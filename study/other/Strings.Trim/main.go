6afe0f582e4a1bb8ab72f6a88129a12cb696bf4b
zjtomoon <zjtomoon@outlook.com>
2022-02-12 20:48:17 +0800

    字符串数组转整形数组

--- study/other/Strings.Trim/main.go
+++ study/other/Strings.Trim/main.go
@@ -4,20 +4,32 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
 )
 
 // golang的strings包中的Trim的用法介绍
-func main()  {
-  input := bufio.NewScanner(os.Stdin)
+func main() {
+	input := bufio.NewScanner(os.Stdin)
 
-  fmt.Println("Please input your words:")
+	fmt.Println("Please input your words:")
 
-  for input.Scan() {
-    result := input.Text()
-    arr1 := strings.Trim(result,";")
-    arr2 := strings.Split(result,";")
-    fmt.Println("arr1 = ",arr1)
-    fmt.Println("arr2 = ",arr2)
-  }
+	for input.Scan() {
+		result := input.Text()
+		arr1 := strings.Trim(result, " ")
+		arr2 := strings.Split(result, " ")
+		fmt.Println("arr1 = ", arr1)
+		fmt.Println("arr2 = ", arr2)
+		fmt.Println(reflect.TypeOf(arr2))
+		var arr3 []int
+
+		//输入的字符串数字转为整形数组
+		for i := 0; i < len(arr2); i++ {
+			tmp, _ := strconv.Atoi(arr2[i])
+			arr3 = append(arr3, tmp)
+		}
+		fmt.Println(arr3)
+		fmt.Println("typeof arr3:", reflect.TypeOf(arr3))
+	}
 }
