a314486e1d01c882416954daa608bbdf504d9cca
zjtomoon <zjtomoon@outlook.com>
2022-02-22 21:48:00 +0800

    切片测试

---
+++ study/other/sliceTest/test01/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [...]int{1, 2, 3, 4, 5, 6}
+	//sli := make([]int,5,5)
+	//fmt.Printf("adress = %p\n",sli)
+	//fmt.Println(sli)
+	sli := arr[:3]
+	fmt.Println("arr = ", arr)
+	fmt.Println("sli = ", sli)
+	fmt.Println("length of sli = ", len(sli))
+	fmt.Println("cap of sli = ", cap(sli))
+
+	// 扩容未超过cap
+	sli = append(sli, 7, 8, 9)
+	fmt.Println("arr = ", arr)
+	//fmt.Printf("address of arr = %p\n",arr)
+	fmt.Println("sli = ", sli)
+	fmt.Printf("address of sli = %p\n", sli)
+
+	//扩容超过cap
+	sli = append(sli, 10)
+	fmt.Println("arr = ", arr)
+	fmt.Println("sli = ", sli)
+	fmt.Printf("address of sli = %p\n", sli)
+}
