f524d3efef5d8008066cf53f9bbbc3b874d1dd06
zjtomoon <zjtomoon@outlook.com>
2022-02-24 10:59:05 +0800

    default commit

---
+++ study/my-demos/buffered_channel/read_closedChannel/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan int, 5)
+	ch <- 1
+	ch <- 2
+	ch <- 3
+	ch <- 4
+	ch <- 5
+
+	//ch <- 6 // deadlock
+
+	//defer close(ch) //deadlock
+	close(ch)
+
+	for val := range ch {
+		fmt.Println(val)
+	}
+	ch <- 6 //panic : send on closed channel
+}
