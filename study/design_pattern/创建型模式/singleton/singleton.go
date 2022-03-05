f3c2867971155e7321a4016c4364956c298e3f21
zjtomoon <zjtomoon@outlook.com>
2022-03-05 19:41:46 +0800

    go设计模式

---
+++ study/design_pattern/创建型模式/singleton/singleton.go
@@ -0,0 +1,19 @@
+package singleton
+
+import "sync"
+
+// Singleton是单例模式类
+
+type Singleton struct{}
+
+var singleton *Singleton
+var once sync.Once
+
+// GetInstance 用于获取单例模式对象
+
+func GetInstance() *Singleton {
+	once.Do(func() {
+		singleton = &Singleton{}
+	})
+	return singleton
+}
