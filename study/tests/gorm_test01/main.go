9945c550323c521e3dff3f3d4d0b2d6412f8ed82
zjtomoon <zjtomoon@outlook.com>
2022-04-19 14:57:50 +0800

    gorm测试

--- study/tests/gorm_test01/main.go
+++ study/tests/gorm_test01/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"log"
 )
 
 var db *gorm.DB
@@ -56,33 +57,69 @@ func init() {
 }
 
 func main() {
+	Search()
+}
 
+func Create() {
 	// create
 	//
-	//db.Table("user2").AutoMigrate(&User{})
-	//db.Table("user3").AutoMigrate(&User{})
+	db.Table("user2").AutoMigrate(&User{})
+	db.Table("user3").AutoMigrate(&User{})
 	//user1 := User{
 	//	Id:   1,
 	//	Age:  19,
 	//	Name: "user1",
 	//}
 
-	//user1 := NewUser(1, 19, "user1")
+	user1 := NewUser(1, 19, "user1")
+	user2 := NewUser(2, 26, "user2")
+	user3 := NewUser(3, 23, "user3")
 	//
-	//db.Table("user2").Create(&user1)
+	err := db.Table("user2").Create(&user1).Error
+	if err != nil {
+		log.Println("err:", err)
+	}
+	err = db.Table("user2").Create(&user2).Error
+	if err != nil {
+		log.Println("err:", err)
+	}
+	err = db.Table("user2").Create(&user3).Error
+	if err != nil {
+		log.Println("err:", err)
+	}
 
 	//db.AutoMigrate(&User{})
+}
 
+func Search() {
 	// search
-	//user := User{}
-	//db.Table("user2").Select("name").Where("id = ?", 1).Find(&user)
-	//fmt.Println(user.Name)
+	user := User{}
+	err := db.Table("user2").Select("name").Where("id = ?", 2).Find(&user).Error
+	if err != nil {
+		log.Println("err:", err)
+	}
+	fmt.Println(user.Id, user.Name, user.Age)
+}
+
+func UpdateTable() {
 
 	// update
-	db.Table("user2").Where("name = ?", "user1").Update("age", 20)
+	err := db.Table("user2").Where("name = ?", "user1").Update("age", 20).Error
+	if err != nil {
+		log.Println("err:", err)
+	}
 	user := User{}
-	db.Table("user2").Select("name", "age").Where("id = ?", 1).Find(&user)
+	err = db.Table("user2").Select("name", "age").Where("id = ?", 1).Find(&user).Error
+	if err != nil {
+		log.Println("err:", err)
+	}
 	fmt.Println("user name :", user.Name)
 	fmt.Println("user age :", user.Age)
+}
 
+func DeleteData() {
+	//delete
+	user := User{}
+	db.Table("user2").Where("id = ?", 1).Delete(&user)
+	fmt.Println(user.Name, user.Age, user.Id)
 }
