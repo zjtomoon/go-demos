d66fc239486cc1210b083dffd7c4435ca48b5ded
zjtomoon <zjtomoon@outlook.com>
2022-05-31 13:25:09 +0800

    文件上传服务

--- study/http/http_upload/client/main.go
+++ study/http/http_upload/client/main.go
@@ -67,3 +67,19 @@ func main() {
 
 	wc.Wait()
 }
+
+
+/**
+在客户端代码中，有几点需要说明一下：
+
+multipart/form-data：在请求头中的Content-Type字段应该设置成multipart/form-data，
+利用表单的形式上传文件。同时还应该加上boundary的内容，boundary可以自己指定。
+
+boundary：如何大家和本文一样自己构建请求体，需要利用boundary进行数据分隔，
+分隔的格式有严格的要求，下面给出一个范例。其中name字段是表单中该元素的名词，
+filename是内容的名称(可以认为是文件名)，二者是有区别的；另外具体的内容，
+如1.png的内容与前一行必须有一个空行(回车)；boundary加上字符"--"作为分隔符，而结束的boundary还应该在末尾加上字符"--"，
+
+todo: 参考 https://zhuanlan.zhihu.com/p/136774587
+
+ */
