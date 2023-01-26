540ce600e9d6c0af2121131e5a3f919bed9a0f9b
zjtomoon <zjtomoon@outlook.com>
2023-01-26 20:30:40 +0800

    interface的坑

--- study/stdlib/other/interface/interface_mistakes01/main.go
+++ study/stdlib/other/interface/interface_mistakes01/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// mistake1()
@@ -29,14 +31,22 @@ type Person struct {
 	Age int
 }
 
-func (p *Person) GetNum() int {
+func (p Person) GetNum() int {
 	return p.Age
 }
 
 func Add(s ...interface{}) int {
 	total := 0
 	for _, v := range s {
-		total += v.(*Person).GetNum()
+		total += v.(Person).GetNum()
+	}
+	return total
+}
+
+func Add2(s ...Person) int {
+	total := 0
+	for _,v := range s {
+		total += v.GetNum()
 	}
 	return total
 }
@@ -45,9 +55,16 @@ func mistake2() {
 	persons := []Person{
 		{18}, {28},
 	}
+	fmt.Println(persons)
+	// fmt.Println(Add(persons...))
+	fmt.Println(Add2(persons...))
+
 	var personArr []interface{}
 	personArr = append(personArr, persons)
 	fmt.Println(personArr...)
+	fmt.Println(personArr[0])
+	// fmt.Println(Add(personArr...)) // interface {} is []main.Person, not main.Person
+	
 }
 
 func TestInterface() {
